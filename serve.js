c8c749d6c8eb23baf55e922039b80a6b3b4f1eac
azadanand9798 <azadanand9798@gmail.com>
2024-04-06 16:16:00 +0530

    orgin point update

--- serve.js
+++ serve.js
@@ -4,8 +4,18 @@ const app = express();
 const mongoose = require('mongoose');
 const path = require('path');
 
+const cors = require('cors');
+
 const connectDB = require('./config/db');
-// connectDB();
+
+
+//cors middleware
+
+const corsOptions = {
+    origin: ['http://127.0.0.1:3001', 'http://127.0.0.1:3000', 'https://fileshear-backend.onrender.com']
+}
+
+app.use(cors(corsOptions));
 
 //Templates
 app.set('views', path.join(__dirname, '/views'));
